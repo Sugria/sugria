aac943f74f8ff2268a36a36789ca0d0b31f1627d
60minuteman <6teeminuteman@gmail.com>
2025-03-02 13:26:25 +0100

    feat: add cloudinary integration for assets and update env config

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,4 +1,4 @@
 export const CLOUDINARY_URLS = {
-  favicon: `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/v1/sugria/favicon-16x16_tz4gkx`,
-  thumbnail: `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload/v1/sugria/og-image_srsysr`
+  favicon: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1/sugria/favicon-16x16_tz4gkx`,
+  thumbnail: `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/v1/sugria/og-image_srsysr`
 } 
